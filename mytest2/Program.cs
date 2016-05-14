04c7cf13feccc68d425597f4f3633873a2e01f1c
pdiemert <pete_diemert@msn.com>
2016-05-14 14:23:29 -0700

    Initial check-in of module mytest2

---
+++ mytest2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+using Griffin.WebServer;
+
+namespace mytest2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var moduleManager = new ModuleManager ();
+
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
